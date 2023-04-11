a537ec9606e47fc2224083dbe7671bc327fb1daa
Abdullah1071 <abdullahshahid1071@gmail.com>
2023-04-11 14:53:02 +0500

    update/fix: remove splash screen after runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:posto/app/app.bottomsheets.dart';
 import 'package:posto/app/app.locator.dart';
 import 'package:posto/firebase_options.dart';
 import 'package:posto/ui/views/app/app_view.dart';
 
 Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -16,4 +18,6 @@ Future main() async {
   setupBottomSheetUi();
 
   runApp(const AppView());
+
+  FlutterNativeSplash.remove();
 }
